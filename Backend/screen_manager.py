0c0e8180898ad7f76e0a211f19a2b90699b2ec50
Proto-Layer <batterykol1337@gmail.com>
2025-08-10 17:38:13 +0300

    Create screen_manager.py

---
+++ Backend/screen_manager.py
@@ -0,0 +1,9 @@
+import kivy 
+
+# Get how many local screens you have
+
+# Get remote screens and specify the type (only phone for now)
+ 
+
+if __name__ == '__main__':
+    print('Screen manager...')
